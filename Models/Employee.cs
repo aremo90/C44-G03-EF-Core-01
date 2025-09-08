f5c30a7f22e01017205f837af09cb51be3c2bc30
aremo90 <mrhamza2005@gmail.com>
2025-09-08 05:52:52 +0300

    04 DbContext

---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    // POCO Class == entity == model
+    internal class Employee
+    {
+        public int Id { get; set; }
+        // Primary Key by default
+        public string? Name { get; set; }
+        public decimal Salary { get; set; }
+        public int Age { get; set; }
+    }
+}
